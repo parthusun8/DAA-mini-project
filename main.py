c17a6a20afe4d813e9131c5c0bd9250f0f6f7f7c
parthusun8 <parthusun8@gmail.com>
2022-06-14 14:45:18 +0530

    Final Invoice generated

--- main.py
+++ main.py
@@ -7,30 +7,15 @@ def main():
 
     ## GET LIBRARY REQUIREMENTS FOR BOOKS
     requirements = read_files.requirement()
-    # print("\nLibrary requirements are -- ")
-    # for book in requirements:
-    #     print(f"\tNeeded {requirements[book]} pieces for {book}")
-
-    ## GET ALL BOOKS AND PRICE OFFERED BY VARIOUS SELLERS
     available_books, no_of_sellers = read_files.sellers(requirements)
-    # print("\n BOOKWISE SELLERS ARE (in sorted order is ) -- ")
-    # for book in available_books:
-    #     print(book)
-    #     for seller in available_books[book]:
-    #         print("\t", seller)
     print(available_books)
 
     unfulfilled, all_selections = selection.select(requirements, available_books)
-    # print("\n BOOKWISE SELLERS ARE (in sorted order is ) -- ")
-    print("\n\n", all_selections)
-    # for book in all_selections:
-    #     print("\n\n", book)
 
     read_files.update_unfulfilled_csv(unfulfilled)
-    ## Generating seller wise data (To be changed when algorithm for selecting books is done)
+
     sellers = invoice(available_books)
 
-    # Creating invoice for each seller
     create_invoice(sellers)
 
 
